484191e1c8f5df8c252bdebb6becec67ad3ffef7
Lee <octal@posteo.net>
2024-07-22 20:56:05 +0800

    fix: Set default theme to light mode

--- web/src/composables/useCookieTheme.ts
+++ web/src/composables/useCookieTheme.ts
@@ -3,7 +3,7 @@ import { useCookies } from '@vueuse/integrations/useCookies'
 export function useCookieTheme() {
   const theme = useCookies(['theme'])
   if (theme.get('theme') === undefined){
-    theme.set('theme', 'system');
+    theme.set('theme', 'light');
   }
   return theme;
 }
